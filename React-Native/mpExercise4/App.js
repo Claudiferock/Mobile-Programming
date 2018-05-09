11e73f22de5e34946bf2d3bfa8887b0f980f07ad
Claudiferock <claudio.rodriguez.vasquez@myy.haaga-helia.fi>
2018-05-09 11:41:17 +0300

    add final versions of exercises 3 and 4

--- React-Native/mpExercise4/App.js
+++ React-Native/mpExercise4/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: '',
+      outputText: '',
+      shoppingList: [],
+    };
+  }
+
+  buttonAdd = () => {
+    const currentItem = this.state.userInput;
+    this.setState({
+      shoppingList: [... this.state.shoppingList, {key: currentItem}, currentItem]
+    });
+  }
+  buttonClear = () => {
+    this.setState({
+      shoppingList: ''
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View  style={styles.inputContainer}>
+        <TextInput
+            style={styles.inputs}
+            placeholder="Write an item you want to add"
+            onChangeText={(userInput) => this.setState({userInput})}
+            value={this.state.userInput}
+          />
+        </View>
+        <View  style={styles.buttons}>
+          <Button style={styles.button} title="Add" onPress={this.buttonAdd} />
+          <Button style={styles.button} title="Clear" onPress={this.buttonClear}/>
+        </View>
+        <Text style={styles.basketTitle}>Shopping List</Text>
+        <View style={styles.list}>
+          <FlatList
+            data={this.state.shoppingList}
+            renderItem={({item}) =>
+              <Text>{item.key}</Text>}
+          />
+        </View>
       </View>
     );
   }
@@ -20,4 +59,37 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputContainer: {
+    width: '100%',
+    flexDirection: "row",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputs: {
+    width: '70%',    
+    padding: 5,
+    margin: 10,
+    borderColor: 'lightgrey',
+    borderWidth: 1.5,
+    alignItems: 'center',
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    width: 120,
+  },
+  button: {
+    padding: 5,
+  },
+  basketTitle: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  list: {
+    height : '30%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',  
+  }
 });
