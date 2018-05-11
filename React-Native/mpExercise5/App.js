0d1193e16ccab428eb28d729e900daa66f99e5f9
Claudiferock <claudio.rodriguez.vasquez@myy.haaga-helia.fi>
2018-05-11 09:55:38 +0300

    Add calculator (with navigation)

--- React-Native/mpExercise5/App.js
+++ React-Native/mpExercise5/App.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
+import { createStackNavigator  } from 'react-navigation';
+import HomeScreen from './components/HomeScreen';
+import HistoryScreen from './components/HistoryScreen';
+
+const MyApp = createStackNavigator({
+  Home: {screen: HomeScreen},
+  History: {screen: HistoryScreen},
+});
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <MyApp/>;
   }
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 30,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputContainer: {
+    width: '90%',
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  inputs: {    
+    width: "40%",
+    padding: 5,
+    margin: 10,
+    borderColor: 'lightgrey',
+    borderWidth: 1.5,
+    alignItems: 'center',
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    width: 150,
+  },
+  button: {
+    padding: 5,
+  },
+  list: {
+    height : '30%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listTitle: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 20,
   },
 });
