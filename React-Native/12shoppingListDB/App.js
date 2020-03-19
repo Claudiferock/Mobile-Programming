59bf9d1f3ebd8d514ca91ff74a0947cfaa74cd10
Claudio <claudifero@gmail.com>
2020-03-19 13:30:46 +0200

    Update RN12

--- React-Native/12shoppingListDB/App.js
+++ React-Native/12shoppingListDB/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 
-const db = SQLite.openDatabase('coursedb.db');
+const db = SQLite.openDatabase('shop_list.db');
 
 export default function App() {
   const [product, setProduct] = useState([]);
@@ -10,34 +10,53 @@ export default function App() {
   const [title, setTitle] = useState('');
 
   useEffect(() => {
-    db.transaction(tx => {
-      tx.executeSql('create table if not exists course (id integer primary key not null, amounts int, title text);');
-    });
+    createDB();
     updateList();    
   }, []);
 
-  // Save course
+  const createDB = () => {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists shopping_list (id integer primary key not null, amounts int, title text);');
+    });
+  }
+
+  // Reset shopping list
+  const resetDB = () => {
+    db.transaction(tx => {
+      try {
+        tx.executeSql('drop table if exists shop_list;');
+        console.log('SUCCESS... Table dropped');
+      }
+      catch {
+        console.log('FAIL!');
+      }
+    });
+    createDB();
+    updateList(); 
+  }
+
+  // Save shopping_list
   const saveItem = () => {
     db.transaction(tx => {
-        tx.executeSql('insert into course (amounts, title) values (?, ?);', [parseInt(amount), title]);    
+        tx.executeSql('insert into shopping_list (amounts, title) values (?, ?);', [parseInt(amount), title]);    
       }, null, updateList
     )
   }
 
-  // Update courselist
+  // Update shopping_listlist
   const updateList = () => {
     db.transaction(tx => {
-      tx.executeSql('select * from course;', [], (_, { rows }) =>
+      tx.executeSql('select * from shopping_list;', [], (_, { rows }) =>
         setProduct(rows._array)
       ); 
     });
   }
 
-  // Delete course
+  // Delete shopping_list
   const deleteItem = (id) => {
     db.transaction(
       tx => {
-        tx.executeSql(`delete from course where id = ?;`, [id]);
+        tx.executeSql(`delete from shopping_list where id = ?;`, [id]);
       }, null, updateList
     )    
   }
@@ -47,9 +66,10 @@ export default function App() {
       <View
         style={{
           height: 5,
-          width: "80%",
-          backgroundColor: "#fff",
-          marginLeft: "10%"
+          width: '100%',
+          borderTopWidth: 2,
+          borderColor: 'rgba(81, 213, 185, .6)',
+          marginVertical: 5,
         }}
       />
     );
@@ -60,20 +80,29 @@ export default function App() {
       <View style={styles.topView}>        
         <Text style={styles.title}>Shopping List</Text>
         <View style={styles.inputsContainer}>
-          <TextInput placeholder='Product...' style={{paddingLeft: 5, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
+          <TextInput placeholder='Product...' style={styles.inputProduct}
             onChangeText={(title) => setTitle(title)}
             value={title}/>  
-          <TextInput placeholder='Amount...' keyboardType="numeric" style={{paddingLeft: 5, fontSize:18, width: 40, borderColor: 'gray', borderWidth: 1}}
+          <TextInput placeholder='qty' keyboardType="numeric" style={styles.inputAmount}
             onChangeText={(amount) => setAmount(amount)}
-            value={amount}/>      
-          <Button onPress={saveItem} title="Add" /> 
+            value={amount}/>  
+{/*           <View style={styles.reset}>
+            <Button onPress={resetDB} title="Reset" color="rgba(225, 40, 10, .9)" /> 
+          </View>    */} 
+          <View style={styles.add}>
+            <Button onPress={saveItem} title="Add" color="rgb(80,225,80)" /> 
+          </View>    
         </View>
       </View>
       <FlatList 
         style={styles.bottomView}
         keyExtractor={item => item.id.toString()} 
-        renderItem={({item}) => <View style={styles.listcontainer}><Text style={{fontSize: 18}}>{item.title}, {item.amounts}</Text>
-        <Text style={{fontSize: 18, color: 'red'}} onPress={() => deleteItem(item.id)}> Bought  </Text></View>} 
+        renderItem={({item}) => 
+          <View style={styles.listcontainer}>
+            <Text style={styles.textProduct}>{item.title}</Text>
+            <Text style={styles.textAmount}>{item.amounts}</Text>
+            <Text style={styles.delete} onPress={() => deleteItem(item.id)}>x</Text>
+          </View>} 
         data={product} 
         ItemSeparatorComponent={listSeparator} 
       />      
@@ -84,35 +113,104 @@ export default function App() {
 const styles = StyleSheet.create({
  container: {
   flex: 1,
-  backgroundColor: '#fff',
   alignItems: 'center',
   justifyContent: 'center',
- },
- topView: {
-  flex:.5,
- },
- title: {
-  color: 'cyan',
-  fontSize: 36,
+},
+topView: {
+  flex:.3,
+  flexDirection:'column',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  width: '100%',
+},
+title: {
+  color: 'darkblue',
+  fontSize: 42,
+  marginTop: 20,
+  width:'100%',
+  textAlign:'center',
   textShadowColor: 'rgba(0,0,0,.6)',
   textShadowOffset: {width: -5, height: 2},
   textShadowRadius: 10,
   textTransform: 'uppercase',
 },
- inputsContainer: {
+inputsContainer: {
+  width: '100%',
   flexDirection: 'row',
-  justifyContent: 'center',
+  justifyContent: 'flex-start',
   alignItems: 'center',
-  alignContent: 'space-between',
-  marginTop: 20,
+},
+inputProduct: {
+  paddingLeft: 5,
+  marginLeft: 30,
+  marginRight: 5,
+  paddingVertical: 2,
+  fontSize: 20,
+  width: 180,
+  color:'white',
+  borderColor: 'darkblue',
+  backgroundColor: 'rgba(0, 20, 145, .7)',
+  borderWidth: 1,
+  borderRadius: 2,
+},
+inputAmount: {
+  paddingLeft: 5,
+  paddingVertical: 2,
+  fontSize:20,
+  width: 40,
+  color:'white',
+  borderColor: 'darkblue',
+  backgroundColor: 'rgba(0, 20, 145, .7)',
+  borderWidth: 1,
+  borderRadius: 2,
+ },
+ reset: {
+  position: 'absolute',
+  right: 80,
+ },
+ add: {
+  position: 'absolute',
+  right: 30,
  },
  bottomView: {
   flex:1,
-  marginLeft : "5%",
+  marginTop:20,
+  width:'100%',
+  paddingHorizontal:30,
  },
  listcontainer: {
   flexDirection: 'row',
-  backgroundColor: '#fff',
   alignItems: 'center'
  },
+ textProduct: {
+  paddingLeft: 2,
+  fontSize: 18,
+  marginRight: 120,
+  color:'rgba(0, 20, 145, .7)',
+ },
+ textAmount: {
+  position: 'absolute',
+  right: 25,
+  fontSize: 8,
+  color: 'darkblue',
+  borderColor:'darkblue',
+  borderWidth:1,
+  width:20,
+  height:20,
+  padding:5,
+  borderRadius:50,
+  textAlign:'center',
+ },
+ delete: {
+  position: 'absolute',
+  right: 0,
+  fontSize: 14,
+  color: 'white',
+  backgroundColor: 'rgba(255, 20, 20, 1)',
+  borderRadius: 50,
+  borderWidth: 1,
+  borderColor: 'rgba(255, 0, 0, 1)',
+  paddingHorizontal: 6,
+  textAlign:'center',
+ }
 });
